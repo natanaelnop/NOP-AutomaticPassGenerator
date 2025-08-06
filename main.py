b26bb6e8fc9f6be903042935bc62196493dd172b
Natanael Pereira <156440427+natanaelnop@users.noreply.github.com>
2025-08-06 20:20:14 -0300

    APRESENTAÇÃO INICIAL DO PROGRAMA

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#teste_de_commit
+print("NOP - Gerador de Senha Automatica");
+print("Voce pode escolher o tamanho da senha que deseja gerar,\n "
+      "se ela contera numeros, letras e caracteres especiais.");
+print("******************************************************")
