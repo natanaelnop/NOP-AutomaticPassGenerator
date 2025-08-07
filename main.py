6bb2ce85644af642d2e220b50810a49a7b3b07c2
Natanael Pereira <156440427+natanaelnop@users.noreply.github.com>
2025-08-06 22:07:55 -0300

    VERSÃO 1.O

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import random
+import string
+caracteres_especiais = "!@#$%^&*()-_=+[]{};:,.<>?/|"
+
 print("NOP - Gerador de Senha Automatica")
 print("Voce pode escolher o tamanho da senha que deseja gerar, \nse ela contera numeros, letras e caracteres especiais.")
 print("******************************************************\n")
@@ -5,3 +9,24 @@ tam = input("Quantos caracteres deseja que sua senha tenha?")
 num = input('Deseja ter numeros em sua senha? (Digite s para sim ou n para não)')
 let = input('Deseja ter letras em sua senha? (Digite s para sim ou n para não)')
 esp = input('Deseja ter caracteres especiais em sua senha? (Digite s para sim ou n para não)')
+
+senha = []
+tipos_escolhidos = []
+
+if num == 's':
+    tipos_escolhidos.append('num')
+if let == 's':
+    tipos_escolhidos.append('let')
+if esp == 's':
+    tipos_escolhidos.append('esp')
+
+while len(senha) < int(tam):
+    tipo = random.choice(tipos_escolhidos)
+    if tipo == 'num':
+        senha.append(str(random.randint(0, 9)))
+    elif tipo == 'let':
+        senha.append(random.choice(string.ascii_lowercase))
+    elif tipo == 'esp':
+        senha.append(random.choice(caracteres_especiais))
+
+print(''.join(senha))
